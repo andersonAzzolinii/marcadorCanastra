10d5da4a71df79fda88bc192c5baba2a100b1f79
andersonAzzolinii <anderson.azzolinii@gmail.com>
2024-07-12 23:54:37 -0300

    - add SQLiteProvider arround the aplication.. used to create db

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,19 @@
+import { SQLiteDatabase, SQLiteProvider, useSQLiteContext } from "expo-sqlite";
 import Home from "./home";
+import { View } from "react-native";
+import migrations from "@/db";
 export default function Index() {
 
+  async function migrateDbIfNeeded(db: SQLiteDatabase) {
+    await migrations(db)
+  }
+
   return (
-    <Home />
+    <View >
+      <SQLiteProvider databaseName="canastra.db" onInit={migrateDbIfNeeded} useSuspense>
+        <Home />
+      </SQLiteProvider>
+    </View>
   );
 }
+
