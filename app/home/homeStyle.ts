ff37820769fdcb7ec8f44049b39d9566c5e3f907
andersonAzzolinii <anderson.azzolinii@gmail.com>
2024-07-13 11:21:33 -0300

    - created listMatches with out get matches.. only with empty render and header render

--- app/home/homeStyle.ts
+++ app/home/homeStyle.ts
@@ -10,4 +10,22 @@ export default StyleSheet.create({
     height: '100%',
     backgroundColor: Colors.light.background
   },
+
+  headerText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+    marginVertical: 15
+  },
+
+  vEmptyMatches: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: "center",
+    marginTop: 30,
+  },
+
+  emptyText: {
+    color: Colors.light.lightText,
+  }
 })
--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,12 +1,42 @@
-import { Text, View } from 'react-native';
+import { Text, View, FlatList, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import homeStyle from './homeStyle';
+import DefaultTextInput from '../Input';
 
 export default function Home() {
+
+  const headerRender = () => {
+    return (
+      <View>
+        <Text style={homeStyle.headerText}>Minhas partidas</Text>
+        <DefaultTextInput
+          placeholder='Procure sua partida aqui'
+        />
+      </View>
+    )
+  }
+
+  const emptyRender = () => (
+    <View style={homeStyle.vEmptyMatches}>
+      <Text style={homeStyle.emptyText}>Nenhuma partida criada </Text>
+    </View>
+  )
+
+  const renderBody = () => {
+    return (
+      <></>
+    )
+  }
   return (
     <SafeAreaView  >
       <View style={homeStyle.container}>
-        <Text>Home page</Text>
+        <FlatList
+          ListHeaderComponent={headerRender}
+          renderItem={renderBody}
+          data={[]}
+          ListEmptyComponent={emptyRender}
+        >
+        </FlatList>
       </View>
     </SafeAreaView>
   )
