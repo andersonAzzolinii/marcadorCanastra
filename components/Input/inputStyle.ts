3cca6a60918db2dbbcd92f185ba39bc3793e228b
andersonAzzolinii <anderson.azzolinii@gmail.com>
2024-07-13 11:45:24 -0300

    - created declarion for images - moved inputComponent

---
+++ components/Input/inputStyle.ts
@@ -0,0 +1,20 @@
+import { Colors } from "@/constants/Colors";
+import { Dimensions, StyleSheet } from "react-native";
+
+export const inputStyles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: 'transparent',
+    backgroundColor: Colors.light.white,
+    borderRadius: 5,
+    width: Dimensions.get('window').width * 0.9,
+    height: 50,
+    paddingLeft: 5,
+  },
+  label: {
+    padding: 5,
+    fontSize: 12,
+    fontWeight: 'bold'
+  }
+
+})
